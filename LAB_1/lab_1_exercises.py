2ed890dc1cb4012a1997c2327a0db628851bf310
fegyobeno <fegyo.benedek@gmail.com>
2025-09-11 13:19:49 +0200

    Updated the git ignore file + added the exercises for the first laboratory

---
+++ LAB_1/lab_1_exercises.py
@@ -0,0 +1,10 @@
+#A következő feleadatokat old meg ciklusok használata nélkül a LAB_1/lab_1_exercises.py alapján:
+
+#1. Hozz létre egy listát, populáld fel általad választott elemekkel, majd írd ki a lista első és utolsó elemét.
+
+#2. Írj egy függvényt, amely bemeneti paraméterként kap egy számot, és visszaadja annak a négyzetét.
+
+#3. Hozz létre egy komplex számot, majd írd ki a valós és képzetes részét külön-külön.
+
+#4. Írj egy függvényt ami kap egy szót és egy szótárat. Amenniyben a szó nem szerepel a kulcsok között, 
+#   akkor add hozzá a szót a szótárhoz és rendelj hozzá egy értéket (pl. 1). Ha már szerepel, akkor növeld meg az értékét eggyel.
